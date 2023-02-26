d95e039773ab268379e61ca1c9aa10836d1679c9
Leandro <leandrolupano@hotmail.com>
2023-02-26 13:25:18 +0100

    ♻️ Add knowledge reducer

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { knowledgeReducer } from "../../feature/reducer/knowledge.reducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    knowledge: knowledgeReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
